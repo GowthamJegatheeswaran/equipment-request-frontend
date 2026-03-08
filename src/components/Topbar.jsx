a0903099f1c43d362663b7bf26b41a8bab3ae69b
Jegatheeswaran Gowtham <jegatheeswarangowtham@gmail.com>
2026-03-08 09:16:19 +0530

    Update Dashboard

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,13 +1,14 @@
 import NotificationBell from "./NotificationBell"
-import logo from "../images/logo.png" // your app icon/logo
 
-export default function Topbar({ title = "Dashboard" }) {
+export default function Topbar({ title }) {
   return (
     <div className="topbar">
       <div className="topbar-left">
-        <img src={logo} alt="Logo" className="topbar-logo" />
+        {/* Direct URL for public folder */}
+        <img src="/images/logo.png" alt="Logo" className="topbar-logo" />
         <span className="topbar-title">{title}</span>
       </div>
+
       <div className="topbar-right">
         <NotificationBell />
       </div>
