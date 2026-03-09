fe19cd0c504911f9d61501b2d2664fd91a3b97bf
Jegatheeswaran Gowtham <jegatheeswarangowtham@gmail.com>
2026-03-09 10:11:38 +0530

    update TO view

--- src/pages/TOApprovalRequests.jsx
+++ src/pages/TOApprovalRequests.jsx
@@ -1,4 +1,4 @@
-import "..styles/toDashboard.css"
+import "../styles/toDashboard.css"
 import Sidebar from "../components/Sidebar"
 import Topbar from "../components/Topbar"
 import { useEffect, useMemo, useState } from "react"
